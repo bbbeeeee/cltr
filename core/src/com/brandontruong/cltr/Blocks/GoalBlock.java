81bebef25b8f6437f63b47a039aa6ccf65f121fd
Brandon Truong <me@brandontruong.com>
2015-05-22 00:37:01 -0500

    GoalBlock added

---
+++ core/src/com/brandontruong/cltr/Blocks/GoalBlock.java
@@ -0,0 +1,24 @@
+package com.brandontruong.cltr.Blocks;
+
+import com.badlogic.gdx.graphics.Color;
+import com.brandontruong.cltr.Block;
+
+/**
+ * Created by btru on 5/20/15.
+ */
+public class GoalBlock extends Block {
+    public final String type = "Goal";
+    public final double attractiveness = 0.0;
+    //public static final Color color = new Color(.9f, .9f, .9f, 1);
+    /**
+     * Standard BlazeBlock constructor. Default attractiveness.
+     * @param x X position
+     * @param y Y position
+     */
+    public GoalBlock(int x, int y){
+        setColor(new Color(.5f, .5f, .5f, 1));
+        setX(x);
+        setY(y);
+        setSymbiosis(0);
+    }
+}
