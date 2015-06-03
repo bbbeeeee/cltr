c4b200bcbdf6ed82b82b6dd292a9206428925772
Brandon Truong <me@brandontruong.com>
2015-06-03 00:35:27 -0500

    Logger easier logging

---
+++ core/src/com/brandontruong/cltr/Logger.java
@@ -0,0 +1,12 @@
+package com.brandontruong.cltr;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Created by btru on 6/3/15.
+ */
+public class Logger {
+    public static void CLTR(String input){
+        Gdx.app.log("CLTR", input);
+    }
+}
