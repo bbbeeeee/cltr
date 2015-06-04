c61e82dcc9cc5f111b9b5d45b4a54a72a76451f8
Brandon Truong <me@brandontruong.com>
2015-06-03 22:20:32 -0500

    Fixed ObstacleBlock

--- core/src/com/brandontruong/cltr/Blocks/ObstacleBlock.java
+++ core/src/com/brandontruong/cltr/Blocks/ObstacleBlock.java
@@ -1,7 +1,24 @@
 package com.brandontruong.cltr.Blocks;
 
+import com.badlogic.gdx.graphics.Color;
+import com.brandontruong.cltr.Block;
+
 /**
  * Created by btru on 6/3/15.
  */
-public class ObstacleBlock {
+public class ObstacleBlock extends Block {
+    public final double attractiveness = 0.0;
+    //public static final Color color = new Color(.9f, .9f, .9f, 1);
+    /**
+     * Standard ObstacleBlock constructor. Default attractiveness.
+     * @param x X position
+     * @param y Y position
+     */
+    public ObstacleBlock(int x, int y){
+        setColor(new Color(1, 1, 1, 1));
+        setX(x);
+        setY(y);
+        setType(EMPTYBLOCK);
+        setSymbiosis(0);
+    }
 }
