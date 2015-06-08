daa4ebccfec79b2a41efdefb0a106057aee96aef
Brandon Truong <me@brandontruong.com>
2015-06-08 10:14:57 -0500

    Created foodblock

---
+++ core/src/com/brandontruong/cltr/Blocks/FoodBlock.java
@@ -0,0 +1,24 @@
+package com.brandontruong.cltr.Blocks;
+
+import com.badlogic.gdx.graphics.Color;
+import com.brandontruong.cltr.Block;
+
+/**
+ * Created by brandon on 6/8/15.
+ */
+public class FoodBlock extends Block {
+    public final double attractiveness = 0.0;
+    //public static final Color color = new Color(.9f, .9f, .9f, 1);
+    /**
+     * Standard GoalBlock constructor. Default attractiveness.
+     * @param x X position
+     * @param y Y position
+     */
+    public FoodBlock(int x, int y){
+        setColor(new Color(.5f, .5f, .5f, 1));
+        setX(x);
+        setY(y);
+        setType(FOODBLOCK);
+        setSymbiosis(0);
+    }
+}
