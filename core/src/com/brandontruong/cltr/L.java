00fd2b2691a530963ec0a0005b8ba91e5cf02308
Brandon Truong <me@brandontruong.com>
2015-06-09 21:19:35 -0500

    Renamed logger

--- core/src/com/brandontruong/cltr/L.java
+++ core/src/com/brandontruong/cltr/L.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Gdx;
 /**
  * Created by btru on 6/3/15.
  */
-public class Logger {
+public class L {
     public static void CLTR(String input){
         Gdx.app.log("CLTR", input);
     }
