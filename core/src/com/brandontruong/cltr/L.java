28ca2cbae68116618d9735ead425f773b4053c00
Brandon Truong <me@brandontruong.com>
2015-06-14 10:02:37 -0500

    fontsize function

--- core/src/com/brandontruong/cltr/L.java
+++ core/src/com/brandontruong/cltr/L.java
@@ -7,10 +7,12 @@ import com.badlogic.gdx.files.FileHandle;
  * Created by btru on 6/3/15.
  */
 public class L {
+    private static final int XSMALL = 0;
     private static final int SMALL = 1;
     private static final int REGULAR = 2;
     private static final int MEDIUM = 3;
     private static final int LARGE = 4;
+    private static final int XLARGE = 5;
 
     public static void CLTR(String input){
         Gdx.app.log("CLTR", input);
@@ -36,17 +38,28 @@ public class L {
         Gdx.app.log("----", "----");
     }
 
-//    public static FileHandle getCorrectFontSize(int size){
-//        switch(size){
-//            case(SMALL):
-//                return Gdx.files.internal("");
-//                break;
-//            case(REGULAR):
-//                break;
-//            case(MEDIUM):
-//                break;
-//            case(LARGE):
-//                break;
-//        }
-//    }
+    public static FileHandle getCorrectFontSize(int size){
+        if(Gdx.graphics.getHeight() > 900){
+            size += 1;
+        }
+
+        switch(size){
+            case(XSMALL):
+                break;
+            case(SMALL):
+                return Gdx.files.internal("fonts/hn_20.fnt");
+            case(REGULAR):
+                return Gdx.files.internal("fonts/hn_32.fnt");
+            case(MEDIUM):
+                return Gdx.files.internal("fonts/hn_48.fnt");
+            case(LARGE):
+                return Gdx.files.internal("fonts/hn_64.fnt");
+            case(XLARGE):
+                return Gdx.files.internal("fonts/hn_100.fnt");
+            default:
+                return Gdx.files.internal("fonts/hn_32.fnt");
+        }
+
+        return Gdx.files.internal("fonts/hn_32.fnt");
+    }
 }
